c1d442218123dda70f1b683828df087812b20b6d
lvidal1 <lvidal910@gmail.com>
2023-05-28 13:26:31 -0500

    Add & setup jest and rtt packages

---
+++ tests/App.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import App from '../src/App';
+
+
+test('Renders main page correctly', () => {
+  // Setup
+  render(<App />)
+
+  // Pre Expectations
+
+  // Init
+
+  // Post Expectations
+  expect(true).toBeTruthy();
+
+});
