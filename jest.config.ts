427fa61d4d3b380be388c3ff8e9e8a9e7ebdde37
lvidal1 <lvidal910@gmail.com>
2023-05-31 17:58:39 -0500

    Update aliases

--- jest.config.ts
+++ jest.config.ts
@@ -10,6 +10,10 @@ export default {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     '^@components/(.*)': '<rootDir>/src/components/$1',
+    '^@pages/(.*)': '<rootDir>/src/pages/$1',
+    '^@hooks/(.*)': '<rootDir>/src/hooks/$1',
+    '^@services/(.*)': '<rootDir>/src/services/$1',
+    '^@store/(.*)': '<rootDir>/src/store/$1',
     '\\.(css|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png|jpg)$': '<rootDir>/__tests__/__mocks__/fileMock.js',
   },
