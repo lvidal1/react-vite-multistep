1c8688fa0a23f4f0e5379b3ec72e9bfb68546952
lvidal1 <lvidal910@gmail.com>
2023-05-31 17:28:31 -0500

    Fix tests

--- __tests__/Layout.test.tsx
+++ __tests__/Layout.test.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter } from 'react-router-dom';
 import { render } from "@testing-library/react"
 import Layout from "@components/Layout"
+
+
+const mockedUsedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...(jest.requireActual("react-router-dom") as any),
+  useNavigate: () => mockedUsedNavigate
+}));
 
 describe("<Layout />", function () {
 
   it("should mount correctly", function () {
-    const screen = render(<Layout />)
+    const screen = render(<BrowserRouter><Layout /></BrowserRouter>)
 
     const layout = screen.getByTestId("layout")
 
