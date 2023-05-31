29cb568941641fed3c1d93b69c89d787212de960
lvidal1 <lvidal910@gmail.com>
2023-05-31 17:43:34 -0500

    Add navigation bar styles

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,30 +3,32 @@ import { useNavigate } from 'react-router-dom';
 import classNames from 'classnames';
 import { IStep } from '../store/types';
 
+import styles from '@styles/components/Navigation.module.scss';
+
 function Navigation() {
   const { steps, step: currentStep } = appStore();
   const navigate = useNavigate();
 
   const getBackground = (step: IStep, _idx: number) => {
-    if (currentStep == _idx) return 'bg-purple-400';
-    if (step.completed) return 'bg-purple-100';
-    return 'bg-purple-500';
+    if (currentStep == _idx) return styles['icon__current'];
+    if (step.completed) return styles['icon__completed'];
+    return styles['icon__next'];
   };
 
   return (
-    <ul className="space-y-3">
+    <ul className={styles.steps}>
       {steps.map((step, _idx) => {
         return (
-          <li key={_idx} className="flex items-center space-x-2 px-2">
-            <span className={classNames('h-4 w-4 rounded-sm', getBackground(step, _idx))}></span>
+          <li key={_idx} className={styles.step}>
+            <span className={classNames(styles.icon, getBackground(step, _idx))}></span>
             {step.completed ? (
               <span
                 onClick={() => navigate(step.path)}
-                className={'text-purple-100 cursor-pointer hover:text-underline'}>
+                className={classNames(styles.label, styles['label__clickable'])}>
                 {step.label}
               </span>
             ) : (
-              <span className={'text-purple-100'}>{step.label}</span>
+              <span className={classNames(styles.label)}>{step.label}</span>
             )}
           </li>
         );
