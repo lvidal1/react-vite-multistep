76e70da46007871b83218883e512966c6d04d2dd
lvidal1 <lvidal910@gmail.com>
2023-05-30 21:36:59 -0500

    Fetch countries and show them in select

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { persist, createJSONStorage } from 'zustand/middleware';
+import { devtools, persist, createJSONStorage } from 'zustand/middleware';
 import { IAppStore, ICountryResponse } from './types';
 import { getCountries } from '../services/countries';
 
@@ -12,30 +12,33 @@ const initialState = {
 
 const getError = (err: any) => err?.message || err?.data?.message || 'Unexpected network error.';
 
-const useAppStore = create(
-  persist<IAppStore>(
-    (set) => ({
-      ...initialState,
-      countries: [],
-      fetchCountries: async () => {
-        try {
-          set(() => ({ isLoading: true }));
-          const countries: ICountryResponse = await getCountries();
-          set(() => ({ countries: countries.data, isLoading: false }));
-        } catch (err: any) {
-          set(() => ({ isLoading: false, error: getError(err) }));
+const appStore = create(
+  devtools(
+    persist<IAppStore>(
+      (set) => ({
+        ...initialState,
+        countries: [],
+        fetchCountries: async () => {
+          try {
+            set(() => ({ isLoading: true }));
+            const countries: ICountryResponse = await getCountries();
+            set(() => ({ countries: countries.data, isLoading: false }));
+          } catch (err: any) {
+            set(() => ({ isLoading: false, error: getError(err) }));
+          }
+        },
+        clear: () => {
+          set(() => initialState);
+          localStorage.clear();
         }
-      },
-      clear: () => {
-        set(() => initialState);
-        localStorage.clear();
+      }),
+      {
+        name: 'app-storage',
+        storage: createJSONStorage(() => localStorage)
       }
-    }),
-    {
-      name: 'app-storage',
-      storage: createJSONStorage(() => localStorage)
-    }
+    ),
+    { anonymousActionType: 'unknown' }
   )
 );
 
-export default useAppStore;
+export default appStore;
