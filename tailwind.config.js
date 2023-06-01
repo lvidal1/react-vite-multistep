9984ee9d4d59afe4b4887621a1077572c8a3a047
lvidal1 <lvidal910@gmail.com>
2023-05-31 23:55:10 -0500

    Add Inter font using vite plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      'sans': ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
+    },
     extend: {
       colors: {
         "purple-100": "#817CA5",
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,64 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Unfonts from 'unplugin-fonts/vite'
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Unfonts({
+    // Google Fonts API V2
+  google: {
+    /**
+     * enable preconnect link injection
+     *   <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+     * default: true
+     */
+    preconnect: false,
+
+    /**
+     * values: auto, block, swap(default), fallback, optional
+     * default: 'swap'
+     */
+    display: 'swap',
+
+    /**
+     * define which characters to load
+     * default: undefined (load all characters)
+     */
+    //text: 'ViteAwsom',
+
+    /**
+     * define where the font load tags should be inserted
+     * default: 'head-prepend'
+     *   values: 'head' | 'body' | 'head-prepend' | 'body-prepend'
+     */
+    injectTo: 'head-prepend',
+
+    /**
+     * Fonts families lists
+     */
+    families: [
+      {
+        /**
+         * Family name (required)
+         */
+        name: 'Inter',
+
+        /**
+         * Family styles
+         */
+        styles: 'wght@400;500;600',
+
+        /**
+         * enable non-blocking renderer
+         *   <link rel="preload" href="xxx" as="style" onload="this.rel='stylesheet'">
+         * default: true
+         */
+        defer: true,
+      },
+    ],
+  },
+   }),],
   resolve:{
     alias: {
       "@": path.resolve(__dirname, "./src"),
